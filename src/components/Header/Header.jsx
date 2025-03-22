c431989861a62333eb1f6557d7e51013d23a6ba0
mwpx <mwpx123@gmail.com>
2025-03-22 20:49:54 +0200

    create folder for separating components and css file for every component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import reactImg from '../assets/react-core-concepts.png';
-
+import reactImg from '../../assets/react-core-concepts.png';
+import './Header.css'
 function Header() {
     const reactDescriptions = ['Fundamental', 'Cruical', 'Core']
     const description = reactDescriptions[genRandomInt(2)];
