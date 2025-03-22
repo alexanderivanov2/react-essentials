7ba99d1591a3b9fe53406386b737670b012a7881
mwpx <mwpx123@gmail.com>
2025-03-22 23:56:54 +0200

    update data.js and add state to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
-import { CORE_CONCEPTS } from './data';
 
 function App() {
+    const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+
+    function handleSelect(selectedButton) {
+        console.log(`Hello World! ${selectedButton}`);
+        setSelectedTopic(selectedButton.toLowerCase());
+    }
+
     return (
         <div>
             <Header />
@@ -24,12 +32,14 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton>Components</TabButton>
-                        <TabButton>JSX</TabButton>
-                        <TabButton>Props</TabButton>
-                        <TabButton>State</TabButton>
+                        <TabButton onSelect={handleSelect}>Components</TabButton>
+                        <TabButton onSelect={handleSelect}>JSX</TabButton>
+                        <TabButton onSelect={handleSelect}>Props</TabButton>
+                        <TabButton onSelect={handleSelect}>State</TabButton>
                     </menu>
+                    { selectedTopic }
                 </section>
+
             </main>
         </div>
     );
--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,7 +1,7 @@
-function TabButton({children}) {
+function TabButton({children, onSelect}) {
     return (
         <li>
-            <button>
+            <button onClick={() => onSelect(children)}>
                 {children}
             </button>
         </li>
